0e5aa28fd999b5503b5b1e58e02ce407b3a14bff
MolruException <molruexception@gmail.com>
2023-03-02 12:02:48 +0900

    Feat: Duck 생성 구현

--- src/main/java/com/molruexception/strategy/Main.java
+++ src/main/java/com/molruexception/strategy/Main.java
@@ -1,7 +1,30 @@
 package com.molruexception.strategy;
 
+import com.google.common.collect.Lists;
+import com.molruexception.strategy.duck.ducks.Duck;
+import com.molruexception.strategy.duck.ducks.FakeQuack;
+import com.molruexception.strategy.duck.ducks.ModelDuck;
+import com.molruexception.strategy.duck.flybehaviors.FlyNoWay;
+import com.molruexception.strategy.duck.quackbehaviors.RubberDuck;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final Duck rubberDuck = new RubberDuck();
+        rubberDuck.setFlyBehavior(new FlyNoWay());
+        rubberDuck.setQuackBehavior(new FakeQuack());
+
+        final Duck modelDuck = new ModelDuck();
+        modelDuck.setFlyBehavior(new FlyNoWay());
+        modelDuck.setQuackBehavior(new FakeQuack());
+
+        final List<Duck> ducks = Lists.newArrayList(rubberDuck, modelDuck);
+        ducks.forEach(duck -> {
+            duck.display();
+            duck.swim();
+            duck.performFly();
+            duck.performQuack();
+        });
     }
 }
